f3f84b18d26417b301a29a4dc98a69cd7267efc3
NormalNerd101 <nhuyhoang1012005@gmail.com>
2024-10-08 15:04:44 +0700

    docs : add lib to requirements.txt

--- setup.py
+++ setup.py
@@ -5,9 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=[
-        'pandas'
-    ],  # Add your dependencies here
+    install_requires=[],  # Add your dependencies here
     entry_points={
         'console_scripts': [
             # Example: 'cli_tool = BTM_Quote_Tool.main:main',
